4915371ecbb4800097b126b1ac63adbf64191602
Darly Hernandez06 <darlyhernadez0624@gmail.com>
2024-06-13 09:52:10 -0500

    trabajando en la clase 13/06/2024

---
+++ app.js
@@ -0,0 +1,12 @@
+// objeto padre en el navegador
+// Crear, acceder, modificar
+let wh = window;
+let dom = document;
+let head = dom.head;
+let body = dom.body;
+
+console.log(wh);
+console.log(dom);
+console.log(head);
+console.log(body);
+
