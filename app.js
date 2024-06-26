698c8aba35096e0754676657155171b3e9c16258
Darly Hernandez06 <darlyhernadez0624@gmail.com>
2024-06-26 08:18:59 -0500

    actualizando trabajo en clase 26/06/2024

--- app.js
+++ app.js
@@ -5,8 +5,30 @@ let dom = document;
 let head = dom.head;
 let body = dom.body;
 
+const $header = dom.querySelector("header");
+const $header_class = dom.querySelector("header");
+const $header_class_all = dom.querySelectorAll(".header");
+const $header_id = dom.querySelector("#header");
+  
 console.log(wh);
 console.log(dom);
 console.log(head);
 console.log(body);
+console.log(dom.title);
+console.log(dom.styleSheets);
+console.log(dom.header);
+console.log($header);
+console.log($header_class);
+console.log($header_class_all);
+console.log($header_id);
 
+
+// anteriormente era asi
+const id = document.getElementById("header");
+const _class = document.getElementsByClassName("header");
+
+console.log(id);
+console.log(_class[0]);
+
+console.clear();
+console.log(body.childNodes);
