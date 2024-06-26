a53af1d92d9092cf7845bae9b4a9cd0a066818df
Darly Hernandez06 <darlyhernadez0624@gmail.com>
2024-06-26 11:31:15 -0500

    actualizando trabajo en clase 26/06/2024

--- NODOS/app.js
+++ NODOS/app.js
@@ -3,11 +3,18 @@ import { usuarios } from "./modulo.js";
 const $root = document.querySelector("#root");
 
 // const $div = document.createElement("div");
-
+// const $p = document.createElement("p");
 // let texto = "Este seria el texto del elemento"
-// $div.textContent = texto;
-// $div.classList.add("box")
-// $root.appendChild($div)
+
+// // $div.textContent = texto;
+
+// $div.classList.add("box");
+
+// $p.textContent = texto;
+// // $p.classList.add("box__texto");
+
+// $root.appendChild($div);
+// $div.appendChild($p);
 
 // const $texto = document.createTextNode("Este seria el texto del elemento");
 
@@ -15,8 +22,10 @@ const $root = document.querySelector("#root");
 usuarios().then((listado) => {
   listado.forEach(usuario => {
     const $div = document.createElement("div");
+    const $h2 = document.createElement("h2");
     $div.classList.add("box")
     $div.textContent = usuario.name;
+    $div.appendChild($h2);
     $root.appendChild($div)
   });
-});
+}).catch();
