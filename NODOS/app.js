d7eaf35386bc3b61385a83f20d0212116744b422
Darly Hernandez06 <darlyhernadez0624@gmail.com>
2024-06-27 11:30:32 -0500

    actualizando del trabajo en clase

--- NODOS/app.js
+++ NODOS/app.js
@@ -2,6 +2,9 @@
 import { usuarios } from "./modulo.js";
 const $root = document.querySelector("#root");
 
+// TEMPLATE
+const $template = document.querySelector("#template").content;
+
 // FRAGMENTOS
 const $card = document.createDocumentFragment();
 
@@ -23,48 +26,55 @@ const $card = document.createDocumentFragment();
 
 
 usuarios().then((listado) => {
+
   listado.forEach(usuario => {
-    const $div = document.createElement("div");
-    $div.classList.add("box", "border"); // agregar estilos
+    // const $div = document.createElement("div");
+    // $div.classList.add("box", "border"); // agregar estilos
 
-    const $pid = document.createElement("p");
-    $pid.textContent = `Id: ${usuario.id}`;
-    $div.appendChild($pid);
+    // const $pid = document.createElement("p");
+    // $pid.textContent = `Id: ${usuario.id}`;
+    // $div.appendChild($pid);
     
-    const $h2 = document.createElement("h2");
-    $h2.textContent = usuario.name;
-    $div.appendChild($h2);
+    // const $h2 = document.createElement("h2");
+    // $h2.textContent = usuario.name;
+    // $div.appendChild($h2);
 
-    const $pUsername = document.createElement("p");
-    $pUsername.textContent = `Username: ${usuario.username}`;
-    $div.appendChild($pUsername);
+    // const $pUsername = document.createElement("p");
+    // $pUsername.textContent = `Username: ${usuario.username}`;
+    // $div.appendChild($pUsername);
 
-    const $pEmail = document.createElement("p");
-    $pEmail.textContent = `Email: ${usuario.email}`;
-    $div.appendChild($pEmail);
+    // const $pEmail = document.createElement("p");
+    // $pEmail.textContent = `Email: ${usuario.email}`;
+    // $div.appendChild($pEmail);
 
-    const $pAddress = document.createElement("p");
-    $pAddress.textContent = `Adress: ${usuario.address.street}, ${usuario.address.suite}, ${usuario.address.city}, ${usuario.address.zipcode} `;
-    $div.appendChild($pAddress);
+    // const $pAddress = document.createElement("p");
+    // $pAddress.textContent = `Adress: ${usuario.address.street}, ${usuario.address.suite}, ${usuario.address.city}, ${usuario.address.zipcode} `;
+    // $div.appendChild($pAddress);
 
-    const $pGeo = document.createElement("p");
-    $pGeo.textContent = `Geo: ${usuario.address.geo.lat}, ${usuario.address.geo.lng} `;
-    $div.appendChild($pGeo);
+    // const $pGeo = document.createElement("p");
+    // $pGeo.textContent = `Geo: ${usuario.address.geo.lat}, ${usuario.address.geo.lng} `;
+    // $div.appendChild($pGeo);
 
-    const $pPhone = document.createElement("p");
-    $pPhone.textContent = `Phone: ${usuario.phone}`;
-    $div.appendChild($pPhone);
+    // const $pPhone = document.createElement("p");
+    // $pPhone.textContent = `Phone: ${usuario.phone}`;
+    // $div.appendChild($pPhone);
 
-    const $pWebsite = document.createElement("p");
-    $pWebsite.textContent = `Website: ${usuario.website}`;
-    $div.appendChild($pWebsite);
+    // const $pWebsite = document.createElement("p");
+    // $pWebsite.textContent = `Website: ${usuario.website}`;
+    // $div.appendChild($pWebsite);
 
-    const $pCompany = document.createElement("p");
-    $pCompany.textContent = `Company: ${usuario.company.name}, ${usuario.company.catchPhrase}, ${usuario.company.bs} `;
-    $div.appendChild($pCompany);
+    // const $pCompany = document.createElement("p");
+    // $pCompany.textContent = `Company: ${usuario.company.name}, ${usuario.company.catchPhrase}, ${usuario.company.bs} `;
+    // $div.appendChild($pCompany);
 
-    $root.appendChild($div);
+    $template.querySelector("h2").textContent = usuario.name;
+    $template.querySelector("div > p").textContent = usuario.email;
+    $template.querySelector("div > p ~ p").textContent = usuario.username;
+
+    let clone = document.importNode($template, true)
+
+    $card.appendChild(clone)
+
   });
-}).catch(error => {
-  console.error('Error fetching users:', error);
-});
+  $root.appendChild($card);
+}).catch();
